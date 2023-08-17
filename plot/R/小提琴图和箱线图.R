aeda629223461779c8b9a7e113fb1848423fbd5b
LAPTOP-9V5P1S5M\promethues <1499237580@qq.com>
2023-08-17 15:11:58 +0800

    删除

--- plot/R/小提琴图和箱线图.R
+++
@@ -1,8 +0,0 @@
-p2 <- ggplot(data = data_plot2,
-  aes(x=index,y=value,fill=index))+
-  geom_violin(scale = 'width')+geom_boxplot(width=0.1)+
-  xlab("year")+ylab("value")+
-  labs(shape=NULL,color=NULL,fill=NULL)+
-  theme_bw()
-
-p2
